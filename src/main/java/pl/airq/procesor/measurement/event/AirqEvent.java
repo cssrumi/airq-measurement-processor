54332e9a458802f9d4757ef8bf71a50bc4a1b77b
Adam Ruminski <cssrumi@gmail.com>
2020-03-12 22:51:08 +0100

    AbstractEvent registered for reflection

--- src/main/java/pl/airq/procesor/measurement/event/AirqEvent.java
+++ src/main/java/pl/airq/procesor/measurement/event/AirqEvent.java
@@ -1,8 +1,10 @@
 package pl.airq.procesor.measurement.event;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
 import java.time.OffsetDateTime;
 import pl.airq.procesor.measurement.payload.Payload;
 
+@RegisterForReflection
 public abstract class AirqEvent implements Event {
 
     final String eventType;
