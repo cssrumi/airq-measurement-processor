77edf6b36d2c20be2f5d0e3c8324c2efd3b4227c
Adam Ruminski <cssrumi@gmail.com>
2020-03-13 18:37:23 +0100

    AirqMeasurementEvent ingore properties added

--- src/main/java/pl/airq/procesor/measurement/event/AirqMeasurementEvent.java
+++ src/main/java/pl/airq/procesor/measurement/event/AirqMeasurementEvent.java
@@ -1,26 +1,17 @@
 package pl.airq.procesor.measurement.event;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import io.quarkus.runtime.annotations.RegisterForReflection;
 import java.time.OffsetDateTime;
 import pl.airq.procesor.measurement.payload.AirqMeasurementPayload;
 
 @RegisterForReflection
+@JsonIgnoreProperties(value = {"eventType"}, allowGetters = true)
 public class AirqMeasurementEvent extends AirqEvent {
 
     AirqMeasurementEvent(OffsetDateTime timestamp, AirqMeasurementPayload payload) {
         super("AirqMeasurement", timestamp, payload);
         System.out.println("AirqMeasurementEvent constructor invoked");
-    }
-
-    @JsonCreator
-    @JsonIgnoreProperties("eventType")
-    public AirqMeasurementEvent from(@JsonProperty("timestamp") OffsetDateTime timestamp,
-                                     @JsonProperty("payload") AirqMeasurementPayload payload) {
-        System.out.println("AirqMeasurementEvent Factory invoked");
-        return new AirqMeasurementEvent(timestamp, payload);
     }
 
     @Override
